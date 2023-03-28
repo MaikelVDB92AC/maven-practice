9fc955be2587fb93a442cf57d25e3672643fd22c
Maikel Van Den Bosch <maikel.van.den.bosch@acerta.be>
2023-03-28 17:06:41 +0200

    small fixes

--- src/main/java/org/mvdb/hello/App.java
+++ src/main/java/org/mvdb/hello/App.java
@@ -3,15 +3,12 @@ import org.apache.logging.log4j.*;
 
 public class App
 {
-    // variables
     private static Logger logger = LogManager.getLogger();
 
-    // methods
     public String sayHello() {
         logger.debug("Somebody called sayHello()");
         return "Hello World";
     }
-
     public static void main(String[] args){
         App app = new App();
         System.out.println(app.sayHello());
